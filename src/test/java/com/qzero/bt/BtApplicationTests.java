619c242fb4f8f6486a1860e065a9896f9a988c78
QZero <2540384328@qq.com>
2020-08-02 11:11:55 +0800

    Initialize project

---
+++ src/test/java/com/qzero/bt/BtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qzero.bt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
